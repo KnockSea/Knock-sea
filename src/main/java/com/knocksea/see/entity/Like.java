ad94d095bafe0df8bae35b111bc1987700ecc1fd
emilywin825 <emilywin825@naver.com>
2023-06-12 16:32:45 +0900

    test

---
+++ src/main/java/com/knocksea/see/entity/Like.java
@@ -0,0 +1,35 @@
+package com.knocksea.see.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder @Entity
+@Table(name="tbl_like")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
+    @Column(length =10)
+    private int heartId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LikeType heartType;
+
+    @Column(nullable = false, length = 10)
+    private int userId;
+
+    @Column(nullable = false, length = 10)
+    private int productId;
+
+    @Column(nullable = false, length = 10)
+    private int classId;
+
+}
