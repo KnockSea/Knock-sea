64b469f883b2af058698fad1e1a8688a4acab294
1gizmo <jdonggwan@naver.com>
2023-06-19 20:11:30 +0900

    23.06.19 / fs 수정 및 mp update

--- fishing/src/main/NsMain.js
+++ fishing/src/main/NsMain.js
@@ -27,6 +27,9 @@ import OwnerCheckMain from './ownercheck/OwnerCheckMain'
 import OwnerCheckShip from './ownercheck/OwnerCheckShip'
 import OwnerCheckFishing from './ownercheck/OwnerCheckFishing'
 import MpQueryText from './mypage/MpQueryText'
+import MpBtInfo from './mypage/MpBtInfo'
+import MpFsInfo from './mypage/MpFsInfo'
+import MpClassInfo from './mypage/MpClassInfo'
 
 
 const NsMain = () => {
@@ -56,36 +59,12 @@ const NsMain = () => {
             {/* <Route path="/ship" element={<OwnerCheckShip/>} ></Route>
             <Route path="/fishing" element={<OwnerCheckFishing/>} ></Route>   */}
             <Route path='/myquery' element={<MpQueryText/>}></Route>
-
+            {/* 업체 정보 */}
+            <Route path='/mpbt' element={<MpBtInfo/>}></Route>
+            <Route path='/mpfs' element={<MpFsInfo/>}></Route>
+            <Route path='/mpclass' element={<MpClassInfo/>}></Route>
         </Routes>
-        {/* <div className='banner'><img src={banner} /></div> */}
-        {/* <NsBanner />
-
-       <div className='mainbox'>
-
-        <div className='contentbox'>
-            <NsItem />
-            <NsItem />
-            <NsItem /> */}
-            {/* <div className='ship'>
-                <div className='title'>
-                    <p className='t1'>오늘의 배낚시 > </p>
-                    <p className='t2'>더보기</p>
-                </div>
-                <div className='shipboxs'>
-                    <div><img src={ex} /></div>
-                    <div><img src={ex} /></div>
-                    <div><img src={ex} /></div>
-                </div>
-            </div> */}
-
-           
-
-        {/* </div>
-        <div className='apibox'>
-            <div className='exbox'>이번주 날씨 > </div>
-        </div>
-       </div> */}
+       
     </section>
   )
 }
@@ -105,7 +84,6 @@ const MainContent = ({ isRouteActive }) => {
 
             </div>
         <div className='apibox'>
-            {/* <div className='exbox'>이번주 날씨 > </div> */}
             <div className='wtbox'>
                 <WeeklyWeather/>
             </div>
