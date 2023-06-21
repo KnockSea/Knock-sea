c6bf4dbe69827ffd300988f2d3daec541db4a326
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-06-21 15:53:54 +0900

    리뷰 detailResponseDTO 수정 (productTitle, eduTitle 추가)

--- fishing/src/main/mypage/MpIqInput.js
+++ fishing/src/main/mypage/MpIqInput.js
@@ -1,46 +1,72 @@
-import React from 'react'
-import MpList from './MpList'
+import React, { useState } from 'react';
+import MpList from './MpList';
 
 const MpIqInput = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const data = {
+      inquiryDetails: content
+    };
+
+    fetch('/api/v1/inquiries', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(result => {
+        // 서버 응답을 처리하는 로직 작성
+        console.log(result);
+      })
+      .catch(error => {
+        // 에러 처리 로직 작성
+        console.error(error);
+      });
+  };
+
   return (
     <section className='MyPageMainBox'>
-    <div className='mainbox1'>
-     <h1>문의하기</h1>
-    
-    <div className='myquerybigbox'>
-    {/* 제목 이름 작성 부분 */}
-        <div className='titlebox '>
+      <div className='mainbox1'>
+        <h1>문의하기</h1>
+      
+        <div className='myquerybigbox'>
+          <div className='titlebox '>
             <div className='clbox'>
-                <div className='cltitle'>제목</div>
+              <div className='cltitle'>제목</div>
             </div>
             <div className='cltextbox'>
-                <input/>
+              <input value={title} onChange={handleTitleChange} />
             </div>
-        </div>
-    {/* 제목 이름 작성 부분  끝 */}
+          </div>
 
-    {/* 내용 작성 부분 */}
-        <div className='titlebox qtbox2'>
+          <div className='titlebox qtbox2'>
             <div className='clbox exx1'>
-                <div className='cltitle'>내용</div>
+              <div className='cltitle'>내용</div>
             </div>
             <div className='cltextbox qttext'>
-              <textarea/>
+              <textarea value={content} onChange={handleContentChange} />
             </div>
+          </div>
+
+          <button className='qtUpdatebtn' onClick={handleSubmit}>작성완료</button>
         </div>
-    {/* 내용 작성 부분 끝 */}
-        
+      </div>
 
-     
+      <MpList />
+    </section>
+  );
+};
 
-
-
-  <button className='qtUpdatebtn'> 작성완료 </button>
-    </div>
-  </div>
-    <MpList/>
-</section>
-  )
-}
-
-export default MpIqInput
+export default MpIqInput;
