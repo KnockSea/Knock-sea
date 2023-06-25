667185c61760122b96d740fae43369fcb9819500
kimjieung97 <dickseung23@naver.com>
2023-06-25 21:02:38 +0900

    # 지승 낚시터 배 / 구분 등록 구현완료

--- fishing/src/main/mypage/MpQueryText.js
+++ fishing/src/main/mypage/MpQueryText.js
@@ -3,6 +3,7 @@ import MpList from './MpList';
 import './MpScss/MpQueryText.scss';
 import { getLoginUserInfo } from '../util/login-util';
 import axios from 'axios';
+import { useEffect } from 'react';
 
 const MpQueryText = () => {
 
@@ -10,15 +11,16 @@ const MpQueryText = () => {
   const [token, setToken] = useState(getLoginUserInfo().token);
   const [images, setImages] = useState([]);
   const [info, setInfo] = useState({
+    category: '' ,
     shipName: '',
-    shipDescription: '',
+    shipDescription: ''
   });
   
   // 이미지 선택상자 하나로 받을때
   const handleImage = e => {
     // const img = Array.from(e.target.files);
     setImages([...e.target.files]);
-    // console.log('이미지 파일 목록 뜸?', img);
+    console.log('이미지 파일 목록 뜸?', images);
   };
 
   const handleTitle = (e) => {
@@ -29,10 +31,22 @@ const MpQueryText = () => {
   const handleContent = (e) => {
     const newInfo = {...info, shipDescription:e.target.value};
     setInfo(newInfo);
+    console.log(info.category);
   };
 
+  const handleCategory = (e) => { 
+    const newInfo = { ...info, category: e.target.value };
+    setInfo(newInfo);
+    console.log(info.category);
+  };
+
+
+  //배/ 낚시터 정보 등록하기
   const shipRegiFetch = async() => {
 
+    // e.preventdefault();
+
+    if(info.category==='ship'){
     const userJsonBlob = new Blob(
       [JSON.stringify(info)],
       { type: 'application/json' }
@@ -61,8 +75,50 @@ const MpQueryText = () => {
         alert(res.text());
       }
     } catch (error) {
-      console.error('씨발');
+      console.error('등록 실패');
     }
+  }else if(info.category==='fishing'){
+
+    const data = {
+      spotName: info.shipName,
+      spotDescription: info.shipDescription
+    }; 
+
+    // console.log(data);
+    const userJsonBlob = new Blob(
+      [JSON.stringify(data)],
+      { type: 'application/json' }
+    );
+
+    console.log(Blob);
+
+    const formData = new FormData();
+    formData.append('spot', userJsonBlob);
+    images.forEach((imgs) => {
+      formData.append('spotImage', imgs);
+    });
+
+    for (let pair of formData.entries()) {
+      console.log('키: ' + JSON.stringify(pair[0]),'밸류: ' + JSON.stringify(pair[1].name));
+    }
+    try{
+      const res = await fetch('http://localhost:8012/api/v1/fishing/register', {
+        method: 'POST',
+        headers: {'Authorization': 'Bearer ' + token},
+        body: formData
+      });
+      
+      if (res.status === 200) {
+        alert('등록 성공');
+      } else if(res.status===400){
+        alert('등록실패');
+      }
+    } catch (error) {
+      console.error('서버와의 통신오류발생');
+    }
+
+  }
+
 
   }
   return (
@@ -71,6 +127,29 @@ const MpQueryText = () => {
      <h1>업체 내용 작성</h1>
     
     <div className='myquerybigbox'>
+    <div className='titlebox'>
+            <div className='clbox'>
+                <div className='cltitle'>카테고리</div>                
+            </div>
+            <div className='category'>
+                <input
+                  id="ship"
+                  type="radio"
+                  value="ship"
+                  checked={info.category === "ship"}
+                  onChange={handleCategory}
+                  />
+              <label htmlFor='ship'>배</label>
+                <input
+                  id="fishing"
+                  type="radio"
+                  value="fishing"
+                  checked={info.category === "fishing"}
+                  onChange={handleCategory}
+                />
+              <label htmlFor='fishing'>낚시터</label>
+            </div>
+   </div>
     {/* 제목 이름 작성 부분 */}
         <div className='titlebox '>
             <div className='clbox'>
