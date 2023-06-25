c055eb5ac6531bf6741803998438c8e9f4d80890
highsky134 <sangchun134@gmail.com>
2023-06-25 13:31:28 +0900

    # front

--- fishing/src/main/NsHeader.js
+++ fishing/src/main/NsHeader.js
@@ -82,18 +82,19 @@ export const NsHeader = () => {
   return (
     <header>
       <div className='header1'>
-        <div className='hdleft'>
-          <Link to={'/'}><img src={logoPath}/></Link>
-            <ul>
-                <li><Link to={'/bt'} style={linkStyle} className='hdleft-tap'> 배낚시</Link></li>
-                <li><Link to={'/fs'}  style={linkStyle} className='hdleft-tap'> 낚시터</Link></li>
-                <li><Link to={'/class'}  style={linkStyle} className='hdleft-tap'> 클래스</Link></li>
-                {userInfo.Grade === 'ADMIN' &&(<li><Link to={'/admin'}>관리자</Link></li>)}
-                {userInfo.token &&(<li><Link to={'/my'}>마이페이지</Link></li>)}
+      <div className='hdleft'>
 
- 
+        <Link to={'/'}><img src={logoPath}/></Link>
+          <ul>
 
-            </ul>
+          <li><Link to={'/bt'} style={linkStyle} className='hdleft-tap'> 배낚시</Link></li>
+            <li><Link to={'/fs'}  style={linkStyle} className='hdleft-tap'> 낚시터</Link></li>
+            <li><Link to={'/class'}  style={linkStyle} className='hdleft-tap'> 클래스</Link></li>
+            <li><Link to={'/admin'}>관리자</Link></li> 
+            {userInfo.Grade === 'ADMIN' &&( 
+            <li><Link to={'/my'}>마이페이지</Link></li>
+            )}
+          </ul>
         </div>
    
         <div className='hdright'>
