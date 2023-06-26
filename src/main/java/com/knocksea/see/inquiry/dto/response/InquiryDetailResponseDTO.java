1443872d10ae5e40a23b17dc08c28aec9104522d
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-06-26 20:21:15 +0900

    관리자 답변완료 누르면 /adminCS 이동

--- src/main/java/com/knocksea/see/inquiry/dto/response/InquiryDetailResponseDTO.java
+++ src/main/java/com/knocksea/see/inquiry/dto/response/InquiryDetailResponseDTO.java
@@ -1,6 +1,7 @@
 package com.knocksea.see.inquiry.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.knocksea.see.inquiry.entity.Answer;
 import com.knocksea.see.inquiry.entity.Inquiry;
 import lombok.*;
 
@@ -26,6 +27,7 @@ public class InquiryDetailResponseDTO {
     private String userName;
 
     private String inquiryTitle;
+//    private String answerDetails;
 
     public InquiryDetailResponseDTO(Inquiry inquiry) {
         this.inquiryId = inquiry.getInquiryId();
@@ -34,6 +36,11 @@ public class InquiryDetailResponseDTO {
         this.userId = inquiry.getUser().getUserId();
         this.userName = inquiry.getUser().getUserName();
         this.inquiryTitle = inquiry.getInquiryTitle();
+//        Answer answer = inquiry.getAnswer();
+//        if (answer != null) {
+//            AnswerDetailResponseDTO dto = new AnswerDetailResponseDTO(answer, inquiry);
+//            this.answerDetails = dto.getAnswerDetails();
+//        }
     }
 }
 
