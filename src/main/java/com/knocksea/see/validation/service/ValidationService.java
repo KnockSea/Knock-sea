80b7385b3276ce29ea4cc96db42a22a8f1c17048
highsky134 <sangchun134@gmail.com>
2023-06-26 19:39:46 +0900

    test

--- src/main/java/com/knocksea/see/validation/service/ValidationService.java
+++ src/main/java/com/knocksea/see/validation/service/ValidationService.java
@@ -70,13 +70,15 @@ public class ValidationService {
         }
     }
 
-    public List<ValidationListResponseDTO> findAllByType(ValidationType validationType, TokenUserInfo userInfo)
+    public List<ValidationListResponseDTO> findAllByType(ValidationType validationType
+//            , TokenUserInfo userInfo
+    )
     throws RuntimeException{
         log.info("validationType : "+" "+validationType);
 
-        if (!(userInfo.getUserGrade().equals("ADMIN"))) {
-            throw new RuntimeException("관리자 기능입니다.");
-        }
+//        if (!(userInfo.getUserGrade().equals("ADMIN"))) {
+//            throw new RuntimeException("관리자 기능입니다.");
+//        }
 /*        User user = userRepository.findById(userId).orElseThrow(()->
                 new RuntimeException("회원 정보가 없습니다"));*/
 
