12ceb450685d0a8e874393f1b487326f04353059
1gizmo <jdonggwan@naver.com>
2023-06-28 21:14:50 +0900

    23.06.28 날씨 포기

--- fishing/src/main/WeeklyWeather.js
+++ fishing/src/main/WeeklyWeather.js
@@ -21,7 +21,55 @@ const WeeklyWeather = () => {
   }
 
   // icons 폴더 내의 모든 이미지 파일 불러오기
-  const icons = Object.values(importAll(require.context("./icons/", false, /\.(png)$/)));
+  // const icons = Object.values(importAll(require.context("./icons/", false, /\.(png)$/)));
+  // icons 폴더 내의 모든 이미지 파일 가져오기
+
+  // const icons = [
+  //   { image: require("./icons/01d.png"), code: "01d" },
+  //   { image: require("./icons/01n.png"), code: "01n" },
+  //   { image: require("./icons/02d.png"), code: "o2d" },
+  //   { image: require("./icons/02n.png"), code: "02n" },
+  //   { image: require("./icons/03d.png"), code: "03d" },
+  //   { image: require("./icons/03n.png"), code: "03n" },
+  //   { image: require("./icons/04d.png"), code: "04d" },
+  //   { image: require("./icons/04n.png"), code: "04n" },
+  //   { image: require("./icons/09d.png"), code: "09d" },
+  //   { image: require("./icons/09n.png"), code: "09n" },
+  //   { image: require("./icons/10d.png"), code: "10d" },
+  //   { image: require("./icons/10n.png"), code: "10n" },
+  //   { image: require("./icons/11d.png"), code: "11d" },
+  //   { image: require("./icons/11n.png"), code: "11n" },
+  //   { image: require("./icons/13d.png"), code: "13d" },
+  //   { image: require("./icons/13n.png"), code: "13n" },
+  //   { image: require("./icons/50d.png"), code: "50d" },
+  //   { image: require("./icons/50n.png"), code: "50n" },
+  //   { image: require("./icons/unknown.png"), code: "unknown" }
+  // ];
+
+  const icons = {
+    "01d": require("./icons/01d.png").default,
+    "01n": require("./icons/01n.png").default,
+    "02d": require("./icons/02d.png").default,
+    "02n": require("./icons/02n.png").default,
+    "03d": require("./icons/03d.png").default,
+    "03n": require("./icons/03n.png").default,
+    "04d": require("./icons/04d.png").default,
+    "04n": require("./icons/04n.png").default,
+    "09d": require("./icons/09d.png").default,
+    "09n": require("./icons/09n.png").default,
+    "10d": require("./icons/10d.png").default,
+    "10n": require("./icons/10n.png").default,
+    "11d": require("./icons/11d.png").default,
+    "11n": require("./icons/11n.png").default,
+    "13d": require("./icons/13d.png").default,
+    "13n": require("./icons/13n.png").default,
+    "50d": require("./icons/50d.png").default,
+    "50n": require("./icons/50n.png").default,
+    "unknown": require("./icons/unknown.png").default
+  };
+  
+
+
 
   useEffect(() => {
     const apiKey = "8507099a51be8da18d7fbb936ef08991";
@@ -70,6 +118,16 @@ const WeeklyWeather = () => {
     setLoading(true);
   };
 
+
+
+  const matchedIcon = icons[icon];
+  const imageSrc = matchedIcon || null;
+
+  console.log("이미지 불러와져라아아아ㅏ!", imageSrc)
+  console.log("하하하하호호호호", matchedIcon)
+  console.log("이런 젠장", icons)
+  console.log("이런 젠장 ㅜㅜㅜ", icon)
+  
   return (
     <div className="weather">
       {loading ? (
@@ -79,6 +137,8 @@ const WeeklyWeather = () => {
           <div className="wttopitem">
             <div className="imgbox111">
               <img src={`https://openweathermap.org/img/w/${icon}.png`} alt={desc} />
+              {imageSrc && <img src={imageSrc} alt="" />}
+
             </div>
             <div className="wtinfo">
               <select onChange={handleCityChange} value={cityName}>
