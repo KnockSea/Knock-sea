8c3f2511363e2525b1efe8dc75d28c6e64e504e2
jisuengkim <dickseung23@naver.com>
2023-06-29 09:48:26 +0900

    # 지승 선박 / 낚시터 / 클래스 페이징 구현

--- fishing/src/main/reservation/RvMain.js
+++ fishing/src/main/reservation/RvMain.js
@@ -21,7 +21,7 @@ function RvMain({fetchFsProduct, FsProduct}) {
   const [products, setProducts] = useState(FsProduct);
   const type = 'SHIP';
 
-  console.log('안녕 나는 rvmain이야 ');
+  // console.log('안녕 나는 rvmain이야 ');
   const handlePageChange = (page) => {
     setPage(page);
     console.log(page);
@@ -33,6 +33,7 @@ function RvMain({fetchFsProduct, FsProduct}) {
       setPage(1);
     }
     setProducts(FsProduct);    
+    setTotalItemCount(products.pageInfo.totalCount);
   },[FsProduct]);
   
   console.log('얘는 존재 해?', FsProduct);
@@ -58,30 +59,18 @@ function RvMain({fetchFsProduct, FsProduct}) {
         </div>
 
         {/* 페이징 버튼 영역 */}
-        <div className='page'>
-        {FsProduct && FsProduct.length > 0 && (
+                            
+        <div className="page">
           <Pagination
-            activePage={page}
-            itemsCountPerPage={size}
-            totalItemsCount={FsProduct.length}
-            pageRangeDisplayed={5}
-            prevPageText={'‹'}
-            nextPageText={'›'}
-            onChange={handlePageChange}
-          />
-        )}
-        {FsProduct && FsProduct.length === 0 && (
-          <Pagination
-            activePage={page}
-            itemsCountPerPage={size}
-            totalItemsCount={0}
-            pageRangeDisplayed={5}
-            prevPageText={'‹'}
-            nextPageText={'›'}
-            onChange={handlePageChange}
-          />
-        )}
-      </div>                              
+          activePage={page}
+          itemsCountPerPage={size}
+          totalItemsCount={totalItemCount}
+          pageRangeDisplayed={5}
+          prevPageText={"‹"}
+          nextPageText={"›"}
+          onChange={handlePageChange}
+          />     
+        </div>                 
       </div>
   )
 }
